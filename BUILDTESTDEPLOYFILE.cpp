1e5d03d550fa6bbbcd853563d40d47d43a0837dd
karthikkadewadi <162300592+karthikkadewadi@users.noreply.github.com>
2024-03-09 13:09:01 +0530

    Update BUILDTESTDEPLOYFILE.cpp

--- BUILDTESTDEPLOYFILE.cpp
+++ BUILDTESTDEPLOYFILE.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 int main() {
     // Print "Hello, world!" to the console
-    cout << "THIS FILE IS FOR TESTING BUILD,TEST,DEPLOY,MY SRN IS PES1UG21CS271"<< endl
+    cout << "THIS FILE IS FOR TESTING BUILD,TEST,DEPLOY,MY SRN IS PES1UG21CS271"<< endl;
     return 0;
 }
